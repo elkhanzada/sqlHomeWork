b1e88435045a7fa95a16bfd624e4f701a4881b16
Dungn <dungduychinguyen@gmail.com>
2021-12-05 04:13:04 +0900

    abdcf

--- store/views.py
+++ store/views.py
@@ -2,10 +2,33 @@ from django.shortcuts import render
 import cx_Oracle
 from elkhan.settings import DATABASES
 import os
+import sys
  
 _user = DATABASES['default']['USER']
 _password = DATABASES['default']['PASSWORD']
 _dsn = DATABASES['default']['HOST']+":"+DATABASES['default']['PORT']
+
+def sqlCount(tableName):
+    res = "select count(*) from " + tableName
+    return res
+
+def sqlGet(tableName, things):
+    res = "select "
+    for i, item in enumerate(things):
+        if(i + 1< len(things)):
+            res = res + item + ", "
+        else:
+            res = res + item + " " 
+    res = res + "from " + tableName
+    return res
+
+def connectToSqlServer():
+    connection = cx_Oracle.connect(
+        user=_user,
+        password=_password,
+        dsn=_dsn)
+    cursor = connection.cursor()
+    return cursor
 
 def getInfo():
     connection = cx_Oracle.connect(
@@ -22,9 +45,11 @@ def getInfo():
 
 # Create your views here.
 def index(request):
-    res = getInfo()
-    #return HttpResponse("Hello, world. You're at the employee index.")
-    randomThing = {'int2k': 10}
+    cursor = connectToSqlServer()
+    cmd = sqlGet("movie", ["movie_id", "picture"])
+    cursor.execute(cmd)
+
+    randomThing = {'int2k': cursor.fetchall()}
     if(request.method == "GET"):
-        print(request.GET.get)
+        print("result of Get is: ", request.GET.get('fname'))
     return render(request, 'templates/index.html', randomThing)
